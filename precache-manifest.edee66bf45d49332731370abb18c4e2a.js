29290c21e5321a6838b2b983e57691969dabc098
albaye <jy317@ic.ac.uk>
2020-10-08 19:24:29 +0100

    Production Build

---
+++ precache-manifest.edee66bf45d49332731370abb18c4e2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c85ea1c377ce2ec55b13a2bdbb6a788",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81e87aa4813a2e877e70",
+    "url": "/static/css/main.6c607aef.chunk.css"
+  },
+  {
+    "revision": "61c88f72367226cb2ab7",
+    "url": "/static/js/2.5b79689a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b79689a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e87aa4813a2e877e70",
+    "url": "/static/js/main.a2b82394.chunk.js"
+  },
+  {
+    "revision": "cdb618858005002cec6c",
+    "url": "/static/js/runtime-main.6a230dc1.js"
+  }
+]);
